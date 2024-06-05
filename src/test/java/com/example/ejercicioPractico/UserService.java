9f6d3819350010c90144dd8f14038f576188d5d9
kevmateo <kevin.cano@epn.edu.ec>
2024-06-04 22:26:58 -0500

    comentarios finales

--- src/test/java/com/example/ejercicioPractico/UserService.java
+++ src/test/java/com/example/ejercicioPractico/UserService.java
@@ -3,10 +3,12 @@ package com.example.ejercicioPractico;
 public class UserService {
     private UserRepository userRepository;
 
+    // Constructor que inyecta el UserRepository
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
     }
 
+    // Método que usa el UserRepository para obtener un usuario por ID
     public User getUserById(String id) {
         return userRepository.findUserById(id);
     }
